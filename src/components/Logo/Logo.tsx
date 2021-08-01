77e21d767f8ddbf367e7f98ac5ce5dc9d7fa1088
Mateus_Kuritza <mateuskuritza@gmail.com>
2021-08-01 19:13:51 -0300

    layout tests with object pdf

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -17,9 +17,12 @@ const Container = styled.div`
     font-size: 62px;
     cursor: default;
     margin-bottom: 25px;
+    text-shadow: 2px 2px 4px rgba(0,0,0,0.4);
+    
     > strong span{
         color: rgba(16,41,120,1);
         font-weight: normal;
     }
     font-weight: bold;
+
 `
--- src/pages/testsByTeacher/TestOption.tsx
+++ src/pages/testsByTeacher/TestOption.tsx
@@ -1,25 +1,63 @@
 import styled from "styled-components";
+import { useState } from "react";
 import ITest from "../../interfaces/ITest";
+import { IoOpenOutline } from "react-icons/io5";
 
 export default function TestOption(props: { key: number, test: ITest }) {
 
     const { test } = props;
+    const [showingPdf, setShowingPdf] = useState(false);
 
     return (
         <Container>
-            <a href={test.pdf} target="_blank" rel="noreferrer" >
+            <p onClick={() => setShowingPdf(!showingPdf)}>
                 {test.name} da disciplina {test.subject.name}
-            </a>
+            </p>
+            {showingPdf &&
+                <div>
+                    <a href={test.pdf} target="_blank" rel="noreferrer">
+                        <IoOpenOutline />
+                    </a>
+                    <object type="application/pdf"
+                        data={test.pdf}
+                        width="400"
+                        height="280">
+                    </object>
+                </div>
+            }
         </Container>
+
     )
 }
 
 const Container = styled.div`
-    background-color: #f3fcff;
-    padding: 4px;
-    margin: 5px;
-    font-weight: 500;
-    :hover{
-        transform: scale(1.05);
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+    > p {
+        text-decoration: underline;
+        margin: 5px;
+        font-weight: 500;
+        :hover{
+            transform: scale(1.05);
+        }
+    }
+
+    div {
+        margin: 5px auto;
+        position: relative;
+        a {
+            position: absolute;
+            left: 15px;
+            top: 15px;
+            *{
+                font-size: 28px;
+                color: #ffffff;
+                :hover{
+                    transform: scale(1.05);
+                }
+            }
+        }
     }
 `;
